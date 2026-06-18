2168695dad12f9aa70c737f6bb1fde7288a9f0a8
yukonlaps90-ship-it <yukonlaps90@gmail.com>
2026-06-18 17:06:00 +0200

    Add JavaScript file

---
+++ script.js
@@ -0,0 +1,7 @@
+// Welcome message
+console.log('Welcome to Guitar Shop!');
+
+// Add any JavaScript functionality here
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Website loaded successfully');
+});
